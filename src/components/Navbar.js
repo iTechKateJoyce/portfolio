c737ebe4a7ba8995067cfae06dd5f0db1272f2c8
katejoyce <katejoyce156@googlemail.com>
2021-04-20 20:19:10 +0100

    nav ammends

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,10 +4,12 @@ import { Link } from 'react-router-dom'
 export default function Navbar() {
   return <div>
     <nav className="navbar is-black">
-    <Link className="navbar-item" to={'/'}>Home</Link>
-      <Link className="navbar-item" to={'/About'}>About</Link>
-      <Link className="navbar-item" to={'/Projects'}>Work</Link>
-      <Link className="navbar-item" to={'/Contact'}>Say hello 👋</Link>
+    <Link className="navbar-item" to={'/'}>
+      <img src="../images/logo.png"/>
+    </Link>
+      <Link id="navItemAbout" className="navbar-item" to={'/About'}>About</Link>
+      <Link id="navItemWork" className="navbar-item" to={'/Projects'}>Work</Link>
+      <Link id="navItemHello" className="navbar-item" to={'/Contact'}>Say hello 👋</Link>
     </nav>
   </div >
 }
