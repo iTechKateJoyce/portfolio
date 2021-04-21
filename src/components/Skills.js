9eeebfe3040678f9ee6b591eda7e5571749ea2b2
katejoyce <katejoyce156@googlemail.com>
2021-04-21 10:21:25 +0100

    test image skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import skills from '../images/Skills.png'
 
-
 export default function Skills() {
   return <section id="skills" className="section">
     <h1 className="title">Skills</h1>
-    <img src={skills} />
+    <img src="https://lh3.googleusercontent.com/MjY4KtGwMknEATYuAT9KkQYbo4GVeqA9L-_4PXX-dKWBpw0VVQXUax9YBggPKU1yStUlQvY-s4_KNChS3V-5IFYm1SA0kWjOBlHzoxeHb6POWmWbFzAHRrX7Wp35FTBLQ99RwgziuAH3Hv7XdSYjabYbqN85dNKQFJevtVFT9cnmNAvm_hiBRSGP_vbda4eD47JzJqQRVqEnTkAS7gUoYLa-7tH5495_BWktqqGgY5Ube9cyhJwr3tSTt6OwA9q80ey2-LALRTxUOFt6mLOq0D_Wx9hIbB9It-rJdf-eZJnRWCiJG-xSKFE0F5k_Wal96htvxhoK3Y4QUVALyDYGTxgWn07R6F0YJe9FqnD46_qxD7plviWQ9NJlVdJ16AurhrFiLZOR9ukETGk2jcfWZ-WegnjVv1UfRj9Ixj1ooVZQqT7Z1qDBty7WcSdO735Txmi-ygZ2lPiLOWVCOpmWuVNe212HMYP-3u32ORMr0idVUvNA7mqQex0fwzwfrKKUVXUId9XC49l5HSp083N3KNMJvwXFfmqVuzQFvGSl7kufPstz8hJ8TcEaHmD_Gaotx3bgquCdMr9I0Mpd4PySwOINKc-lq-PQHjYO5FujGvpw184zTlGPmGsCmBqX-V5OjgouqSim46_jar0UI5wB5xBo-bN6UTkKFczuB08bHw0mU_18Rfkhy-yYhBHCNfyDI1lt606wy5ejKJJw3X09GWpgOA=w93-h204-no?authuser=0" />
   </section>
 }
