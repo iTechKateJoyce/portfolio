703b93ab20138a604559cffc20ee26c92c24058b
katejoyce <katejoyce156@googlemail.com>
2021-04-21 07:44:40 +0100

    file-loader

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import logo from '../images/logo.png'
 
 export default function Navbar() {
   return <div>
     <nav className="navbar is-black">
     <Link id="navItemHome" className="navbar-item" to={'/'}>
-      <img src="../images/logo.png"/>
+      {/* <img src="../images/logo.png"/> */}
+      <img src={logo}/>
     </Link>
       <Link id="navItemAbout" className="navbar-item" to={'/About'}>About</Link>
       <Link id="navItemWork" className="navbar-item" to={'/Projects'}>Work</Link>
