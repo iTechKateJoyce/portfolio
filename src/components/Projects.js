5f685f5c1ae0af9b48a5b346447bec0b635382f3
katejoyce <katejoyce156@googlemail.com>
2021-05-19 12:26:30 +0100

    *

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 import Squiggle from '../images/squiggle.svg'
 import { SiGithub } from 'react-icons/si'
 import { FaRocket } from 'react-icons/fa'
